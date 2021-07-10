cbd7261b8f3465cf6c2135817d404125c0d67f48
youkeoo1 <1921380844@qq.com>
2021-07-10 11:26:10 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue';
+// 引入移动端组件库
+import Vant from 'vant';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+// 手机端适配
+import 'lib-flexible';
+// 引入移动端组件库样式文件
+import 'vant/lib/index.css';
 
+Vue.use(Vant);
 Vue.config.productionTip = false;
 
 new Vue({
